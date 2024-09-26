4bed269a281b79965bdec3b25c96258b760a876c
Arghya Chakrabarty <arghya2801@gmail.com>
2024-09-26 13:31:01 +0000

    Made the grid into a dynamically adjusting sqaure.

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-grid_size = 16;
+grid_size = 25;
+let grid_container = document.getElementById("outer-square");
+
+function calculateDivWidth() {
+    let container_width =  grid_container.offsetWidth;
+    return Math.floor(container_width / Math.sqrt(grid_size)) - 10;
+}
+
+box_size = calculateDivWidth() + "px";
+
 for (let i=0; i<grid_size; i++){
     var box = document.createElement("div");
-    box.style.margin = "5px";
-    box.style.border = "2px solid black";
-    box.style.background = "red";
     box.innerHTML = "Hello World";
+    box.style.width = box_size;
+    box.style.height = box_size;
+    // box.style.width = "200px";
     
     document.getElementById("outer-square").appendChild(box);
 }
