dd9111c2e8b7c4ba9e49519095dae7aaf62ce86a
Arghya Chakrabarty <arghya2801@gmail.com>
2024-09-26 13:56:17 +0000

    Added mouseover color changes.

--- script.js
+++ script.js
@@ -8,12 +8,22 @@ function calculateDivWidth() {
 
 box_size = calculateDivWidth() + "px";
 
+
 for (let i=0; i<grid_size; i++){
     var box = document.createElement("div");
+    box.classList.add("boxes");
     box.innerHTML = "Hello World";
     box.style.width = box_size;
     box.style.height = box_size;
     // box.style.width = "200px";
+    
+    box.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = "#5ebfea";
+    });
+    
+    box.addEventListener("mouseout", (event) => {
+        event.target.style.backgroundColor = ""; // Reset to default background color
+    });
     
     document.getElementById("outer-square").appendChild(box);
 }
