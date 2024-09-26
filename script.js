b804ec64985b2444fac7ad9dc8d4a7cffdeace28
Arghya Chakrabarty <arghya2801@gmail.com>
2024-09-26 14:25:02 +0000

    Button creates new empty grid of use input size

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-grid_size = 25;
-let grid_container = document.getElementById("outer-square");
+// grid_size = 25;
 
-function calculateDivWidth() {
-    let container_width =  grid_container.offsetWidth;
-    return Math.floor(container_width / Math.sqrt(grid_size)) - 10;
+function makeGrid(grid_size){
+    let grid_container = document.getElementById("outer-square");
+    
+    function calculateDivWidth() {
+        let container_width =  grid_container.offsetWidth;
+        return Math.floor(container_width / Math.sqrt(grid_size)) - 10;
+    }
+    
+    box_size = calculateDivWidth() + "px";
+
+    for (let i=0; i<grid_size; i++){
+        var box = document.createElement("div");
+        box.classList.add("boxes");
+        box.innerHTML = "Hello World";
+        box.style.width = box_size;
+        box.style.height = box_size;
+        // box.style.width = "200px";
+        
+        box.addEventListener("mouseover", (event) => {
+            event.target.style.backgroundColor = "#5ebfea";
+        });
+        
+        // box.addEventListener("mouseout", (event) => {
+        //     event.target.style.backgroundColor = "";
+        // });
+        
+        document.getElementById("outer-square").appendChild(box);
+    }
 }
 
-box_size = calculateDivWidth() + "px";
-
-function makeGrid(){
-    console.log("hello");
-}
-
-for (let i=0; i<grid_size; i++){
-    var box = document.createElement("div");
-    box.classList.add("boxes");
-    box.innerHTML = "Hello World";
-    box.style.width = box_size;
-    box.style.height = box_size;
-    // box.style.width = "200px";
-    
-    box.addEventListener("mouseover", (event) => {
-        event.target.style.backgroundColor = "#5ebfea";
-    });
-    
-    // box.addEventListener("mouseout", (event) => {
-    //     event.target.style.backgroundColor = "";
-    // });
-    
-    document.getElementById("outer-square").appendChild(box);
-}
 
 function gridSizeInput() {
     grid_size_input = prompt("Enter grid size: ");
@@ -38,8 +38,10 @@ function gridSizeInput() {
 
 let grid_button = document.getElementById("button1");
 grid_button.addEventListener("click", () => {
-    console.log("click success");
-    gridSizeInput();
+    document.getElementById("outer-square").textContent = "";
+    grid_size = gridSizeInput();
+    total_boxes = grid_size * grid_size;
+    makeGrid(total_boxes);
 })
 
 // function getHumanChoice(){
